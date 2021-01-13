33f6253c762a8b3939696a81c0369ca74ee0cb81
DahyeonL <45159242+DahyeonL@users.noreply.github.com>
2021-01-14 01:36:34 +0900

    Update and rename DahyeonL/Main_B_S2_15486_퇴사2.java to DahyeonL/1월3주/Main_B_S2_15486_퇴사2.java

--- DahyeonL/1월3주/Main_B_S2_15486_퇴사2.java
+++ DahyeonL/1월3주/Main_B_S2_15486_퇴사2.java
@@ -1,5 +1,12 @@
-package algo_study_2021;
-
+/*
+ * -퇴사 2-
+ * 뒤에서 부터 일을 할 수 있을 때와 없을 때를 구분해서 최대값을 구한다.
+ * 일을 할 수 있을 경우에는 일을 했을 때 값과 일을 하지 않았을 때의 값을 비교해서 최대값을 구한다.
+ * 
+ * 메모리 : 313724KB
+ * 시간 : 660ms
+ * 풀이 시간 : 3H
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
