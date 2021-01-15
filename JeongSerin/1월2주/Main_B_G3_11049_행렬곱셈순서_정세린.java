b888500a5fa4d4db49958e5e1a76a37ec199d9cf
JeongSerin <53457254+JeongSerin@users.noreply.github.com>
2021-01-15 15:05:51 +0900

    1월2주_B_G3_11049_행렬곱셈순서_정세린.java

--- JeongSerin/1월2주/Main_B_G3_11049_행렬곱셈순서_정세린.java
+++ JeongSerin/1월2주/Main_B_G3_11049_행렬곱셈순서_정세린.java
@@ -1,6 +1,6 @@
 /*
- 23712KB
- 624ms
+ 22280KB
+ 380ms
  2h
  DP 분할정복, memo[a][b] : a~b까지 연산 횟수 최솟값 저장
  유사한 문제: B_G3_11066_파일합치기
@@ -15,22 +15,11 @@ import java.util.StringTokenizer;
 
 public class Main_B_G3_11049_행렬곱셈순서_정세린 {
 	
-	public static class Matrix{	// 행렬
-		int r;
-		int c;
-		
-		public Matrix(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
-	}
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());	// 행렬의 개수
-		Matrix[] M = new Matrix[N];		// 입력된 행렬의 크기
 		int[][] memo = new int[N][N];	// n1 ~ n2까지 곱셈연산의 최솟값
-		Matrix[][] size = new Matrix[N][N];	// n1~n2 곱 결과 행렬의 크기를 저장
+		int[][][] size = new int[N][N][2]; 	//[n][n][0]: r, [n][n][1]: c
 		
 		for (int i = 0; i < memo.length; i++) {
 			Arrays.fill(memo[i], Integer.MAX_VALUE);
@@ -39,13 +28,14 @@ public class Main_B_G3_11049_행렬곱셈순서_정세린 {
 		StringTokenizer st = null;
 		for (int n = 0; n < N; n++) {
 			st = new StringTokenizer(br.readLine());
-			M[n] = new Matrix(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-			size[n][n] = new Matrix(M[n].r, M[n].c);
+			size[n][n][0] = Integer.parseInt(st.nextToken());
+			size[n][n][1] = Integer.parseInt(st.nextToken());
 			memo[n][n] = 0;
 			
 			if (n > 0) {	
-				memo[n-1][n] = M[n-1].r * M[n-1].c * M[n].c;	// n-1, n곱셈 연산 수
-				size[n-1][n] = new Matrix(M[n-1].r, M[n].c);	// n-1, n곱셈 후 행렬 크기
+				memo[n-1][n] = size[n-1][n-1][0] * size[n-1][n-1][1] * size[n][n][1];	// n-1, n곱셈 연산 수
+				size[n-1][n][0] = size[n-1][n-1][0];
+				size[n-1][n][1] = size[n][n][1];	// n-1, n곱셈 후 행렬 크기
 			}
 		}	// end of input
 		
@@ -58,10 +48,11 @@ public class Main_B_G3_11049_행렬곱셈순서_정세린 {
 				
 				for (int d = i; d < j; d++) {	// i~d, d~j
 					int tmp = memo[i][d] + memo[d+1][j];	// i~d + d~j 각 연산 횟수 합
-					tmp += size[i][d].r * size[i][d].c * size[d+1][j].c; 	// 두개를 곱했을 떄 연산 횟수 더함
+					tmp += size[i][d][0] * size[i][d][1] * size[d+1][j][1]; 	// 두개를 곱했을 떄 연산 횟수 더함
 					if (tmp < memo[i][j]) {	// 전보다 작은 값이면
 						memo[i][j] = (memo[i][j] < tmp) ? memo[i][j] : tmp;	// 다시 저장	
-						size[i][j] = new Matrix(size[i][d].r, size[d+1][j].c);	// 해당 값에 대한 행렬의 크기 저장
+						size[i][j][0] = size[i][d][0];	// 해당 값에 대한 행렬의 크기 저장
+						size[i][j][1] = size[d+1][j][1];	
 					}
 				}
 			}
