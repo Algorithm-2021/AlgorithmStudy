e4f4950e23f9dcc7e7fd218c8de6977de9e29eab
imdaeyong <45128072+imdaeyong@users.noreply.github.com>
2021-02-05 16:40:50 +0900

    2월 1주 Main_B_S1_1389_케빈베이컨의6단계법칙.java

---
+++ imdaeyong/2월 1주/Main_B_S1_1389_케빈베이컨의6단계법칙.java
@@ -0,0 +1,52 @@
+package DP;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author 김대용
+ * 메모리 :14624kb
+ * 실행시간 : 132ms
+ * 푸는시간 : 1H
+ *
+ */
+public class Main_B_S1_1389_케빈베이컨의6단계법칙 {
+	static int map[][] = new int[101][101];
+	static int N, M, ans;
+	static int Min = Integer.MAX_VALUE;
+
+	public static void main(String[] args) throws Exception {		
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				if (i != j)
+					map[i][j] = 999999;
+		for (int j = 0; j < M; j++) {
+			int row = sc.nextInt() - 1;
+			int col = sc.nextInt() - 1;
+			map[row][col] = 1;
+			map[col][row] = 1;
+		}
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (map[i][j] > map[i][k] + map[k][j])
+						map[i][j] = map[i][k] + map[k][j];
+				}
+			}
+		}
+		for (int i = 0; i < N; i++) {
+			int temp = 0;
+			for (int j = 0; j < N; j++) {
+				temp += map[i][j];
+			}
+			if (Min > temp) {
+				Min = temp;
+				ans = i;
+			}
+		}
+		System.out.println(ans + 1);
+	}
+}
