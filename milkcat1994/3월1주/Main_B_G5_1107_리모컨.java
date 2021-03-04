0779ce177a91179054d8a74650a8390dd2b5fbdf
milkcat1994 <milkcat1994@gmail.com>
2021-03-04 15:33:23 +0900

    Edit 3월1주_G5_1107_리모컨_milkcat1994

--- milkcat1994/3월1주/Main_B_G5_1107_리모컨.java
+++ milkcat1994/3월1주/Main_B_G5_1107_리모컨.java
@@ -5,10 +5,10 @@ import java.util.StringTokenizer;
 
 /*
  * -리모컨-
- * 1. 현재값, 100번[초기위치]에서 방향키만 눌러서 이동가능한 값, 가능한 번호[i]에서 방향키로 이동가능한 값 중 최소값 도출(Line 30)
+ * 1. 현재값, 가능한 번호[i]에서 방향키로 이동가능한 값 중 최소값 도출(Line 31)
  * 
- * 메모리 : 71196KB
- * 시간 : 316ms
+ * 메모리 : 44312KB
+ * 시간 : 248ms
  * 풀이 시간 : 1H 30M
  */
 
@@ -25,10 +25,11 @@ public class Main_B_G5_1107_리모컨 {
 	}
 	
 	static int solve() {
+		// 초기위치 100번에서 방향키로 이동시 값.
 		int res = Math.abs(N-100);
 		for(int i=0; i<MAX; ++i) {
 			if(isAble(i)) {
-				res = Math.min(res, Math.min(Math.abs(i-100)+Math.abs(N-i), getLength(i)+Math.abs(N-i)));
+				res = Math.min(res, getLength(i)+Math.abs(N-i));
 			}
 		}
 		
@@ -37,12 +38,11 @@ public class Main_B_G5_1107_리모컨 {
 
 	// 활성 버튼에 따라 해당 버튼 누를 수 있는지 반환
 	static boolean isAble(int num) {
-		String str = Integer.toString(num);
-		int length = str.length();
+		if(num==0) if(!button[0]) return false;
 		
-		for(int i=0; i<length; ++i) {
-			if(!button[str.charAt(i)-'0'])
-				return false;
+		while(num>0) {
+			if(!button[num%10]) return false;
+			num /= 10;
 		}
 		return true;
 	}
