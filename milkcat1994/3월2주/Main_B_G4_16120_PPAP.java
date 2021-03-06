72ba731aed76f2a0d1554d1f5857e376913e52ae
milkcat1994 <milkcat1994@gmail.com>
2021-03-06 12:40:59 +0900

    Edit 3월2주_G4_16120_PPAP_milkcat1994

--- milkcat1994/3월2주/Main_B_G4_16120_PPAP.java
+++ milkcat1994/3월2주/Main_B_G4_16120_PPAP.java
@@ -3,16 +3,17 @@ import java.io.InputStreamReader;
 
 /*
  * -PPAP-
+ * 1. 처음에는 StringBuilder를 이용하여 length를 줄여가는 식으로 풀이하였음.
+ * 2. 후에 for문 이용하여 'A'의 나타남 유무에 따른 처리로 풀이하였음.
  * 
- * 메모리 : 23648KB
- * 시간 : 424ms
+ * 메모리 : 20772KB
+ * 시간 : 272ms
  * 풀이 시간 : 1H
  */
 
 //출처 : https://www.acmicpc.net/problem/16120
 public class Main_B_G4_16120_PPAP {
 	static String originStr;
-	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws Exception {
 		init();
@@ -21,24 +22,32 @@ public class Main_B_G4_16120_PPAP {
 	}
 	
 	static String solve() {
-		int patternLength = "PPAP".length();
+		boolean isPossible = true;
 		int originStrLength = originStr.length();
+		
+		int pCnt = 0;
 		for(int i=0; i<originStrLength; ++i) {
-			sb.append(originStr.charAt(i));
-			if(sb.length() >= patternLength) {
-				isPPAP();
+			switch (originStr.charAt(i)) {
+			case 'P':
+				pCnt++;
+				break;
+
+			case 'A':
+				if(pCnt>=2 && i<originStrLength-1 && originStr.charAt(i+1)=='P') {
+					pCnt--;
+					i++;
+				}
+				else {
+					isPossible = false;
+				}
+				break;
+			default:
+				// do Nothing
+				break;
 			}
 		}
 		
-		return sb.toString().equals("P") ? "PPAP" : "NP";
-	}
-	
-	static void isPPAP() {
-		int idx = sb.length();
-		if(sb.charAt(idx-4) == 'P' && sb.charAt(idx-3) == 'P' && sb.charAt(idx-2) == 'A' && sb.charAt(idx-1) == 'P') {
-			sb.setLength(sb.length()-4);
-			sb.append('P');
-		}
+		return (isPossible && pCnt==1) ? "PPAP" : "NP";
 	}
 	
 	static void init() throws Exception {
