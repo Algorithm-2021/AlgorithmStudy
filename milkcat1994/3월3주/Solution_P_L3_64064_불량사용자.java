872afe372e5186fe3f9e480e935f9a168c34e6d3
milkcat1994 <milkcat1994@gmail.com>
2021-03-16 15:45:27 +0900

    Update 3월3주_L3_64064_불량사용자_milkcat1994

--- milkcat1994/3월3주/Solution_P_L3_64064_불량사용자.java
+++ milkcat1994/3월3주/Solution_P_L3_64064_불량사용자.java
@@ -1,95 +1,52 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /*
  * -불량사용자-
+ * 1. 첫 풀이는 hashMap과 set을 이용하여 사전에 선택가능한 user_id를 선별하여 처리하였으나
+ * 2. dfs 함수내에서 idx를 바로 불러와 사용가능하여 hashMap을 제외하였다.
+ * 3. 또한 user_id의 수가 8개 이하임을 고려하여 bitMask가 가능하여 중복 처리가 더 쉬워졌다.
  * 
- * 테스트 1 〉	통과 (20.52ms, 53.4MB)
- * 테스트 2 〉	통과 (19.37ms, 53.5MB)
- * 테스트 3 〉	통과 (21.26ms, 53.3MB)
- * 테스트 4 〉	통과 (18.71ms, 53.2MB)
- * 테스트 5 〉	통과 (292.70ms, 86.4MB)
- * 테스트 6 〉	통과 (43.35ms, 55.2MB)
- * 테스트 7 〉	통과 (21.56ms, 53.7MB)
- * 테스트 8 〉	통과 (21.21ms, 53.8MB)
- * 테스트 9 〉	통과 (19.84ms, 53.5MB)
- * 테스트 10 〉	통과 (22.44ms, 54.3MB)
- * 테스트 11 〉	통과 (23.23ms, 53.3MB)
+ * 테스트 1 〉	통과 (0.54ms, 52.8MB)
+ * 테스트 2 〉	통과 (1.57ms, 53.2MB)
+ * 테스트 3 〉	통과 (1.62ms, 52.9MB)
+ * 테스트 4 〉	통과 (1.59ms, 52.2MB)
+ * 테스트 5 〉	통과 (236.38ms, 102MB)
+ * 테스트 6 〉	통과 (24.08ms, 54.1MB)
+ * 테스트 7 〉	통과 (1.55ms, 51.4MB)
+ * 테스트 8 〉	통과 (1.86ms, 52.7MB)
+ * 테스트 9 〉	통과 (2.52ms, 53.8MB)
+ * 테스트 10 〉	통과 (1.02ms, 52.5MB)
+ * 테스트 11 〉	통과 (3.06ms, 52.2MB)
  * 풀이 시간 : 40M
  */
 
 //출처 : https://programmers.co.kr/learn/courses/30/lessons/64064
 public class Solution_P_L3_64064_불량사용자 {
-	static boolean[] isSelected;
-	static List<List<Integer>> list;
-	static Set<String> set;
-	static int answer = 0;
+	static Set<Integer> set;
 	
 	public int solution(String[] user_id, String[] banned_id) {
-		HashMap<String, Integer> hm = new HashMap<>();
-		// ban_id에 해당하는 user_id 리스트를 저장하기 위한 리스트
-		list = new ArrayList<List<Integer>>();
-		set = new HashSet<String>();
+		set = new HashSet<>();
 		
-		for(int i=0; i<user_id.length; ++i) {
-			hm.put(user_id[i], i);
-		}
+		dfs(0, user_id, banned_id, 0);
 		
-		// 해당 아이디가 선택 되었는지 확인
-		isSelected = new boolean[user_id.length];
-		
-		// ban_id형태를 정규표현식으로 바꾸기 위한 작업
-		String[] convertBannedId = new String[banned_id.length];
-		for(int i=0; i<banned_id.length; ++i) {
-			convertBannedId[i] = banned_id[i].replaceAll("\\*", ".");
-			list.add(new ArrayList<>());
-		}
-		
-		// 정규표현식 이용한 매칭 되는 id 미리 구성
-		for(int regexIdx=0; regexIdx<convertBannedId.length; ++regexIdx) {
-			for(int i=0; i<user_id.length; ++i) {
-				if(user_id[i].matches("^"+convertBannedId[regexIdx]+"$")) {
-					list.get(regexIdx).add(hm.get(user_id[i]));
-				}
-			}
-		}
-		
-		dfs(0, 0, banned_id.length, "");
-		
-		answer = set.size();
-		return answer;
+		return set.size();
 	}
 	
-	void dfs(int idx, int cnt, int maxCnt, String str){
+	void dfs(int idx, String[] user_id, String[] banned_id, int isSelected){
 		// 모두 골랐다면 set으로 중복 제거
-		if(cnt==maxCnt) {
-			String[] sArr = str.split("");
-			Arrays.sort(sArr, (a,b) -> a.compareTo(b));
-			StringBuilder sb = new StringBuilder();
-			for(String ts : sArr)
-				sb.append(ts);
-			set.add(sb.toString());
+		if(idx==banned_id.length) {
+			set.add(isSelected);
 			return;
 		}
 		
-		List<Integer> tempList = list.get(idx);
-		int idIdx;
-		for(int j=0; j<tempList.size(); ++j) {
-			idIdx = tempList.get(j);
-			if(isSelected[idIdx]) continue;
+		String regex = banned_id[idx].replace("*", ".");
+		for(int userIdIdx=0; userIdIdx<user_id.length; ++userIdIdx) {
+			if((isSelected & (1<<userIdIdx)) > 0 || !user_id[userIdIdx].matches(regex)) continue;
 			
-			isSelected[idIdx]=true;
-			
-			dfs(idx+1, cnt+1, maxCnt, str+idIdx);
-			
-			isSelected[idIdx]=false;
+			dfs(idx+1, user_id, banned_id, isSelected | (1<<userIdIdx));
 		}
 	}
-
 
 	public static void main(String[] args) {
 		Solution_P_L3_64064_불량사용자 sol = new Solution_P_L3_64064_불량사용자();
