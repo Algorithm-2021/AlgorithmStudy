ac5f9e69862adf07e80eb6a72b29a6ba990435e6
JeongSerin <53457254+JeongSerin@users.noreply.github.com>
2021-03-24 00:39:05 +0900

    Solution_P_L2_64062_징검다리건너기_정세린.java

--- JeongSerin/3월4주/Solution_P_L2_64062_징검다리건너기_정세린.java
+++ JeongSerin/3월4주/Solution_P_L2_64062_징검다리건너기_정세린.java
@@ -24,17 +24,17 @@ public class Solution_P_L2_64062_징검다리건너기_정세린 {
 	static class Solution {
 		class Point { // 징검다리
 			int idx, num;
-
+	
 			public Point(int idx, int num) {
 				this.idx = idx;
 				this.num = num;
 			}
 		}
-
+	
 		public int solution(int[] stones, int k) { // k:최대로 건너뜃수있는 칸수
 			int answer = 0; // 건널수 있는 니니즈 수
 			int len = stones.length; // 징검다리 길이
-
+	
 			PriorityQueue<Point> pq = new PriorityQueue<Point>(new Comparator<Point>() {
 				@Override
 				public int compare(Point o1, Point o2) {
@@ -43,7 +43,7 @@ public class Solution_P_L2_64062_징검다리건너기_정세린 {
 			});
 			for (int i = 0; i < len; i++) // 징검다리의 숫자가 가장 작은것 부터 확인
 				pq.offer(new Point(i, stones[i]));
-
+	
 			int zeroCnt = 0; // 연속으로 있는 0의 수
 			while (!pq.isEmpty()) {
 				int num = pq.peek().num; // 가장 작은 징검다리의 숫자
@@ -51,7 +51,7 @@ public class Solution_P_L2_64062_징검다리건너기_정세린 {
 				L: while (!pq.isEmpty() && pq.peek().num == num) { // 징검다리숫자가 작은것들 0으로 만들기
 					int idx = pq.poll().idx;
 					stones[idx] = 0;
-
+	
 					// 0이 연속으로 k개인지 확인하는 과정
 					zeroCnt = 1;
 					// idx위치에서 앞방향으로 0의 개수
@@ -60,10 +60,8 @@ public class Solution_P_L2_64062_징검다리건너기_정세린 {
 					for (int i = idx + 1; i <= front; i++) {
 						if (stones[i] == 0) {
 							zeroCnt++;
-							if (zeroCnt >= k)
-								break L;
-						} else
-							break;
+							if (zeroCnt >= k) break L;
+						} else break;
 					}
 					
 					// idx위치에서 뒷방향으로 0의개수 확인
@@ -72,10 +70,8 @@ public class Solution_P_L2_64062_징검다리건너기_정세린 {
 					for (int i = idx - 1; i >= back; i--) {
 						if (stones[i] == 0) {
 							zeroCnt++;
-							if (zeroCnt >= k)
-								break L;
-						} else
-							break;
+							if (zeroCnt >= k) break L;
+						} else break;
 					}
 				}
 				if (zeroCnt >= k) { // 연속으로 있는 0의 수가 k이상이면 더이상 못건넘
@@ -83,7 +79,7 @@ public class Solution_P_L2_64062_징검다리건너기_정세린 {
 					break;
 				}
 			} // end of while
-
+	
 			return answer;
 		}
 	} // end of Solution
