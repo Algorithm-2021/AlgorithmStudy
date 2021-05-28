9ca13bd5818fff6c0f8bdc4e5b634852a7a7cba3
JeongSerin <53457254+JeongSerin@users.noreply.github.com>
2021-05-28 20:03:06 +0900

    Update Solution_P_L1_42839_소수찾기_정세린.java

--- JeongSerin/5월4주/Solution_P_L1_42839_소수찾기_정세린.java
+++ JeongSerin/5월4주/Solution_P_L1_42839_소수찾기_정세린.java
@@ -1,9 +1,9 @@
 /*
-테스트 8 〉	통과 (14.21ms, 55.5MB)
-테스트 9 〉	통과 (0.09ms, 52.9MB)
-테스트 10 〉	통과 (71.75ms, 52.6MB)
-테스트 11 〉	통과 (9.88ms, 53.5MB)
-테스트 12 〉	통과 (1.70ms, 52.6MB)
+테스트 8 〉	통과 (11.69ms, 54.2MB)
+테스트 9 〉	통과 (0.15ms, 52.3MB)
+테스트 10 〉	통과 (11.74ms, 52.8MB)
+테스트 11 〉	통과 (1.63ms, 52MB)
+테스트 12 〉	통과 (1.44ms, 52.3MB)
  * 40m
  * 순열, 소수확인
  * https://programmers.co.kr/learn/courses/30/lessons/42839?language=java
@@ -20,22 +20,22 @@ public class Solution_P_L1_42839_소수찾기_정세린 {
 		boolean[] selected;
 		StringBuilder sb = new StringBuilder(); // 만들어진 숫자
 		HashSet<Integer> set = new HashSet<>();
-
+	
 		public int solution(String numbers) {
 			int answer = 0;
 			int len = numbers.length();
 			nums = numbers;
 			selected = new boolean[len];
-
+	
 			for (int i = 1; i <= len; i++) { // 1 ~ len 개
 				sb.setLength(0);
 				permutation(len, i);
 			}
-
+	
 			answer = set.size();
 			return answer;
 		} // end of solution()
-
+	
 		void permutation(int n, int r) {
 			if (r == 0) {
 				// 소수 확인
@@ -43,7 +43,7 @@ public class Solution_P_L1_42839_소수찾기_정세린 {
 				if (isPrime(selectedNum)) set.add(selectedNum);
 				return;
 			}
-
+	
 			for (int i = 0; i < n; i++) {
 				char number = nums.charAt(i);
 				if (selected[i]) continue;
@@ -54,15 +54,14 @@ public class Solution_P_L1_42839_소수찾기_정세린 {
 				sb.deleteCharAt(sb.length() - 1);
 			}
 		} // end of permutation()
-
+	
 		boolean isPrime(int n) {
 			if (n <= 1) return false;
-			if (n == 2) return true;
-			for (int i = 2; i <= n / 2; i++) 
+			for (int i = 2; i <= (int)Math.sqrt(n); i++) 
 				if (n % i == 0) return false;
 			return true;
 		} // end of isPrime();
-
+	
 	}
 
 	public static void main(String[] args) {
